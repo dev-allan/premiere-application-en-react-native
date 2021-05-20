ee042c6db1da7dea300084485c236996b600067e
allan duminy <allanduminy@protonmail.com>
2021-05-20 13:39:38 +0200

    création du composant accueil, faire composant savoir faire

--- src/component/accueil.js
+++ src/component/accueil.js
@@ -5,7 +5,12 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default Accueil = () => {
     return(
     <View style={styles.container}>
-        <Image source={require("")}/>
+        <Image style={styles.logo} source={require("../img/Logo.png")}/>
+        <Image style={styles.logoPeinture80} source={require("../img/peinture80.png")}/>
+        <Text style={styles.titre}>Artisan Peintre à Amiens depuis 1996</Text>
+        <Text style={styles.presentation}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit ultricies massa eget fermentum. Quisque vulputate feugiat consequat. In rhoncus dui a ligula iaculis molestie. Vestibulum elit ipsum, pharetra non mi convallis, bibendum gravida lacus.
+        </Text>
     </View>
     )
 }
@@ -15,5 +20,25 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 24,
         backgroundColor: "#E47070"
+    },
+
+    logo: {
+        alignSelf: "center",
+    },
+
+    logoPeinture80: {
+        marginTop: 50,
+    },
+
+    titre: {
+        marginTop: 15,
+        alignSelf: 'center',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+
+    presentation: {
+        marginTop: 15,
+        textAlign: 'justify',
     }
 })
