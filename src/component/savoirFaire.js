d9347abd5cccb0752b535232e2d6defb7ded93a2
allan duminy <allanduminy@protonmail.com>
2021-05-26 16:43:03 +0200

    ajout du carousel

--- src/component/savoirFaire.js
+++ src/component/savoirFaire.js
@@ -1,11 +1,15 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { StyleSheet, Text, View, Image, StatusBar, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, ScrollView, Dimensions } from 'react-native';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from '../../carouselCard';
+import data from '../../contentRealisation.json';
 
 export default SavoirFaire = () => {
     return(
     <ScrollView style={styles.container}>
         <StatusBar/>
+        <CarouselCards />
         <View style={styles.blockView}>
             <View style={styles.block}>
                 <Image style={styles.image} source={require('../img/mur.jpg')}/>
@@ -43,6 +47,45 @@ export default SavoirFaire = () => {
     </ScrollView>
     )
 }
+
+//test carousel
+const CarouselCards = () => {
+    const [index, setIndex] = React.useState(0)
+    const isCarousel = React.useRef(null)
+  
+    return (
+      <View>
+          <Carousel
+        layout="tinder"
+        layoutCardOffset={9}
+        ref={isCarousel}
+        data={data}
+        renderItem={CarouselCardItem}
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={ITEM_WIDTH}
+        onSnapToItem={(index) => setIndex(index)}
+        useScrollView={true}
+      />
+      <Pagination
+        dotsLength={data.length}
+        activeDotIndex={index}
+        carouselRef={isCarousel}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          marginHorizontal: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.92)'
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+        tappableDots={true}
+      />
+      </View>
+    )
+};
+
+//////
 
 const styles = StyleSheet.create({
     container: {
